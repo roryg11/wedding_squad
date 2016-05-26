22bbba4e8608fa0f20e3fb03ffd8f6535e2b0918
roryg11 <Rory.C.Grant@gmail.com>
2016-05-26 09:02:24 -0700

    switching over to postgresql in development

--- app/controllers/concerns/api/base_controller.rb
+++ app/controllers/concerns/api/base_controller.rb
@@ -1,10 +1,3 @@
 class Api::BaseController < ApplicationController
   respond_to :json
-
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-    respond_with @current_user
-  end
-
 end
