9e84fe9bc96bf7a407d6043471ee13fbdcb5b229
roryg11 <Rory.C.Grant@gmail.com>
2016-06-09 18:41:30 -0700

    setting up email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,17 @@ Rails.application.configure do
     }
   }
 
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :doman                => "gmail.com",
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
